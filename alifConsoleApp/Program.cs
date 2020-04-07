abcbbe7fbae163f1053017ca16cd853aabed829c
azick7553 <azick7553@gmail.com>
2020-04-07 19:26:24 +0500

    init project

---
+++ alifConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using classLibProjects;
+namespace alifConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Person p = new Person(1, "Test");
+            p.GetInfo();
+
+        }
+    }
+}
---
+++ classLibProjects/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace classLibProjects
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+
+        public Person(int Id, string FullName)
+        {
+            this.Id = Id;
+            this.FullName = FullName;
+        }
+        public Person()
+        {
+
+        }
+        public void GetInfo()
+        {
+            System.Console.WriteLine($"ID: {this.Id}\nFullName: {this.FullName}");
+        }
+    }
+}
